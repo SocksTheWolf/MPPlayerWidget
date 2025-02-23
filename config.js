b9186fa186f6756613fb4b989597e1425caebe81
SocksTheWolf <132639882+SocksTheWolf@users.noreply.github.com>
2025-02-23 12:25:26 -0800

    Add placeholder names

--- config.js
+++ config.js
@@ -8,9 +8,9 @@ const config = {
 	// Format:
 	// {name: "PLAYER NAME", char: "character they are playing. If the character has a space in their name, replace with _"},
 	// Every line should end with a comma
-	// INSERT PLAYERS AFTER THIS LINE
-	{name: "Socks", char: "yoshi"},
-	{name: "Blur", char: "luigi"},
-    {name: "AK", char: "toadette"},
-	{name: "Sharkycharr", char: "peach"},
+	// INSERT OR MODIFY PLAYERS AFTER THIS LINE
+	{name: "Player 1", char: "yoshi"},
+	{name: "Player 2", char: "luigi"},
+    {name: "Player 3", char: "toadette"},
+	{name: "Player 4", char: "peach"},
 ]};
